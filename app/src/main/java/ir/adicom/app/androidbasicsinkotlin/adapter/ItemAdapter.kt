49eefb0c4ccd6196445dce9f5f1356b395d65cee
adicomdotir <adicom.ir@gmail.com>
2021-10-04 23:53:13 +0330

    Add cardview in recyclerview

--- app/src/main/java/ir/adicom/app/androidbasicsinkotlin/adapter/ItemAdapter.kt
+++ app/src/main/java/ir/adicom/app/androidbasicsinkotlin/adapter/ItemAdapter.kt
@@ -4,14 +4,16 @@ import android.content.Context
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.ImageView
 import android.widget.TextView
 import androidx.recyclerview.widget.RecyclerView
 import ir.adicom.app.androidbasicsinkotlin.R
 import ir.adicom.app.androidbasicsinkotlin.model.Affirmation
 
 class ItemAdapter(val context: Context, val dataset: List<Affirmation>): RecyclerView.Adapter<ItemAdapter.ItemViewHolder>() {
-    class ItemViewHolder(private val itemView: View): RecyclerView.ViewHolder(itemView) {
+    class ItemViewHolder(itemView: View): RecyclerView.ViewHolder(itemView) {
         val textView = itemView.findViewById<TextView>(R.id.item_title)
+        val imageView = itemView.findViewById<ImageView>(R.id.item_image)
     }
 
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ItemViewHolder {
@@ -22,6 +24,7 @@ class ItemAdapter(val context: Context, val dataset: List<Affirmation>): Recycle
     override fun onBindViewHolder(holder: ItemViewHolder, position: Int) {
         val item = dataset[position]
         holder.textView.text = context.resources.getString(item.stringResourceId)
+        holder.imageView.setImageResource(item.imageResourceId)
     }
 
     override fun getItemCount(): Int {
