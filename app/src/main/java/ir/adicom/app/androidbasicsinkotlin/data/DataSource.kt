d65bd7fbd1101ffe644870767c5dd2fbd7dd2286
adicomdotir <adicom.ir@gmail.com>
2021-10-05 11:42:29 +0330

    Add final code of section

--- app/src/main/java/ir/adicom/app/androidbasicsinkotlin/data/DataSource.kt
+++ app/src/main/java/ir/adicom/app/androidbasicsinkotlin/data/DataSource.kt
@@ -1,21 +1,64 @@
+/*
+* Copyright (C) 2021 The Android Open Source Project.
+*
+* Licensed under the Apache License, Version 2.0 (the "License");
+* you may not use this file except in compliance with the License.
+* You may obtain a copy of the License at
+*
+*     http://www.apache.org/licenses/LICENSE-2.0
+*
+* Unless required by applicable law or agreed to in writing, software
+* distributed under the License is distributed on an "AS IS" BASIS,
+* WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+* See the License for the specific language governing permissions and
+* limitations under the License.
+*/
 package ir.adicom.app.androidbasicsinkotlin.data
 
+import ir.adicom.app.androidbasicsinkotlin.model.Dog
 import ir.adicom.app.androidbasicsinkotlin.R
-import ir.adicom.app.androidbasicsinkotlin.model.Affirmation
 
-class DataSource {
-    fun loadAffirmations(): List<Affirmation> {
-        return listOf(
-            Affirmation(R.string.affirmation1, R.drawable.image1),
-            Affirmation(R.string.affirmation2, R.drawable.image2),
-            Affirmation(R.string.affirmation3, R.drawable.image3),
-            Affirmation(R.string.affirmation4, R.drawable.image4),
-            Affirmation(R.string.affirmation5, R.drawable.image5),
-            Affirmation(R.string.affirmation6, R.drawable.image6),
-            Affirmation(R.string.affirmation7, R.drawable.image7),
-            Affirmation(R.string.affirmation8, R.drawable.image8),
-            Affirmation(R.string.affirmation9, R.drawable.image9),
-            Affirmation(R.string.affirmation10, R.drawable.image10)
+/**
+ * An object to generate a static list of dogs
+ */
+object DataSource {
+
+    val dogs: List<Dog> = listOf(
+        Dog(
+            R.drawable.tzeitel,
+            "Tzeitel",
+            "7",
+            "sunbathing"
+        ),
+        Dog(
+            R.drawable.leroy,
+            "Leroy",
+            "4",
+            "sleeping in dangerous places"
+        ),
+        Dog(
+            R.drawable.frankie,
+            "Frankie",
+            "2",
+            "stealing socks"
+        ),
+        Dog(
+            R.drawable.nox,
+            "Nox",
+            "8",
+            "meeting new animals"
+        ),
+        Dog(
+            R.drawable.faye,
+            "Faye",
+            "8",
+            "Digging in the garden"
+        ),
+        Dog(
+            R.drawable.bella,
+            "Bella",
+            "14",
+            "Chasing sea foam"
         )
-    }
+    )
 }
